2b73c79e6c16c97140c4ada073ca096c449c57ac
klimalik <klimamalik14@gmail.com>
2024-02-06 21:07:10 +0700

    api: edit & hapus data siswa

--- routes/siswaRoute.js
+++ routes/siswaRoute.js
@@ -3,33 +3,42 @@ const router = express.Router();
 const prisma = require('../libs/prisma')
 
 router.post('/add', async (req, res) => {
-  const {image, username, tanggal_lahir, email, notel, jabatan, nama_ortu, alamat} = req.body
+  const {idUbah, image, username, tanggal_lahir, email, notel, jabatan, nama_ortu, alamat} = req.body
 
   try {
-    const siswaByEmail = await prisma.siswa.findUnique({
-      where: {
-        email: email,
-      },
-    })
-    console.log({siswaByEmail});
-
-    if(!siswaByEmail) {
-      await  prisma.siswa.create({
-        data: {
-            image, 
-            username, 
-            tanggal_lahir, 
-            email, 
-            notel, 
-            jabatan, nama_ortu, 
-            alamat
-        }
+    if(idUbah) {
+      await prisma.siswa.update({
+        where: {
+          id: idUbah
+        },
+        data: {image, username, tanggal_lahir, email, notel, jabatan, nama_ortu, alamat}
       });
-      res.status(201).json({ status: true, message: '🚀 Siswa Berhasil Ditambah'})
+      res.status(201).json({ status: true, message: '🚀 Siswa Berhasil Diubah'})
     } else {
-      res.status(404).json({ status: false, message: 'Email Sudah Digunakan' });
+      const siswaByEmail = await prisma.siswa.findUnique({
+        where: {
+          email: email,
+        },
+      })
+      
+      if(!siswaByEmail) {
+        await prisma.siswa.create({
+          data: {
+              image, 
+              username, 
+              tanggal_lahir, 
+              email, 
+              notel, 
+              jabatan, nama_ortu, 
+              alamat
+          }
+        });
+        res.status(201).json({ status: true, message: '🚀 Siswa Berhasil Ditambah'})
+  
+      } else {
+        res.status(404).json({ status: false, message: 'Email Sudah Digunakan' });
+      }
     }
-
 
   } catch (error) {
     console.log(error);
@@ -41,10 +50,43 @@ router.get('/get', async (req, res) => {
   try {
     const siswa = await prisma.siswa.findMany();
     res.status(201).json({ status: true, message: 'Semua Data Siswa', data: siswa})
-    // console.log({siswa});
   } catch (error) {
     console.log(error);
     res.status(500).json({ status: false, message: 'Maaf, Terjadi Kesalahan Teknis' });
+  }
+})
+
+router.get('/get-siswa/:id', async (req, res) => {
+  const siswaId = req.params.id;
+  try {
+    const siswa = await prisma.siswa.findUnique({
+      where: {
+        id: parseInt(siswaId)
+      }
+    });
+
+    res.status(201).json({ status: true, message: 'Semua Data Siswa', data: siswa})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: false, message: 'Maaf, Terjadi Kesalahan Teknis' });
+  }
+})
+
+router.get('/delete/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const siswaDelete = await prisma.siswa.delete({
+      where: {
+        id: parseInt(id)
+      }
+    });
+
+    res.status(201).json({ status: true, message: `🚀 ${siswaDelete.username} Berhasil DiHapus`})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: false, message: 'Maaf, Terjadi Kesalahan Teknis' });
+  } finally {
+    await prisma.$disconnect();
   }
 })
 
